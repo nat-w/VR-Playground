6d0a446a58eb12b0505e3e6e9378b83b04e1a2de
Natalia Whiteley <nataliawhiteley11@gmail.com>
2018-10-03 12:18:51 -0400

    Initial Commit

---
+++ Assets/Scripts/ControllerGrab.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrab : MonoBehaviour {
+	private SteamVR_TrackedObject trackedObj;
+	// Object that controller is colliding with
+	private GameObject collidingObj;
+	// Object that player is currently grabbing
+	private GameObject objInHand;
+
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index);}
+	}
+	
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	// Check if colliding object exists and has a rigid body
+	private void SetCollidingObject(Collider other) {
+		if (collidingObj || !other.GetComponent<Rigidbody>()) {
+			return;
+		}
+		collidingObj = other.gameObject;
+	}
+
+	// Sets up object as potential grab target
+	public void OnTriggerEnter(Collider other) {
+		SetCollidingObject(other);
+	}
+	// Object is still grab target when controller hovers
+	public void OnTriggerStay(Collider other) {
+		SetCollidingObject(other);
+	}
+	// Remove target once controller leaves
+	public void OnTriggerExit(Collider other) {
+		if (!collidingObj) {
+			return;
+		}
+		collidingObj = null;
+	}
+
+	// Creates a joint between the controller and object
+	public void GrabObject() {
+		objInHand = collidingObj;
+		collidingObj = null;
+
+		var joint = AddFixedJoint();
+		joint.connectedBody = objInHand.GetComponent<Rigidbody>();
+	}
+
+	// Creates a fixed joint on the controller
+	public FixedJoint AddFixedJoint() {
+		FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+		fx.breakForce = 20000;
+		fx.breakTorque = 20000;
+		return fx;
+	}
+
+	// Destroys joint
+	private void ReleaseObject() {
+		if (GetComponent<FixedJoint>()) {
+			return;
+		}
+		// Destroys the joint, freeing the object
+		GetComponent<FixedJoint>().connectedBody = null;
+		Destroy(GetComponent<FixedJoint>());
+
+		// Changes velcity of object (throwing) 
+		objInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+		objInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+		objInHand = null;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Grabs object when trigger is pressed down
+		if (Controller.GetHairTriggerDown()) {
+			if (collidingObj) {
+				GrabObject();
+			}
+		}
+		// Drops object when trigger is released
+		if (Controller.GetHairTriggerUp()) {
+			if (objInHand) {
+				ReleaseObject();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+	
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// This is the position of the finger on the touch pad
+		if (Controller.GetAxis() != Vector2.zero) {
+			Debug.Log(Controller.GetAxis());
+		}
+
+		// This is how you get input from the trigger
+		if (Controller.GetHairTriggerDown()) {
+			Debug.Log("Trigger Press");
+		}
+		if (Controller.GetHairTriggerUp()) {
+			Debug.Log("Trigger Release");
+		}
+
+		// This is how you get input from the grip button
+		if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log("Grip Button Press");
+		}
+		if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log("Grip Button Press");
+		}
+	}
+}
